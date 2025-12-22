e61c4ec4d123a9c7aad363debda4604cf752efc4
juny02 <qapl12321@gmail.com>
2025-12-22 13:16:35 +0900

    chore(core): add default values in setting

--- core/setting.py
+++ core/setting.py
@@ -1,13 +1,14 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    MONGO_PORT: int
-    MONGO_URI: str
-    MONGO_DB_NAME: str
-    MONGO_ROOT_USERNAME: str
-    MONGO_ROOT_PASSWORD: str
-    OPEN_AI_API_KEY: str
+    MONGO_PORT: int = Field(alias="MONGO_PORT", default=123)
+    MONGO_URI: str = Field(alias="MONGO_URI", default="url")
+    MONGO_DB_NAME: str = Field(alias="MONGO_DB_NAME", default="name")
+    MONGO_ROOT_USERNAME: str = Field(alias="MONGO_ROOT_USERNAME", default="user")
+    MONGO_ROOT_PASSWORD: str = Field(alias="MONGO_ROOT_PASSWORD", default="pw")
+    OPEN_AI_API_KEY: str = Field(alias="OPEN_AI_API_KEY", default="key")
 
     class Config:
         env_file = ".env.dev"
