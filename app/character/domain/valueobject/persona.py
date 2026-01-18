db81be08dcc4ff8221e5ce8e76a7e9b25abd0f44
juny02 <qapl12321@gmail.com>
2026-01-18 21:48:05 +0900

    chore(character): add todo

--- app/character/domain/valueobject/persona.py
+++ app/character/domain/valueobject/persona.py
@@ -5,7 +5,7 @@ from app.character.domain.enum import Gender, Purpose, Style, Tone
 
 @dataclass(frozen=True)
 class Persona:
-    gender: Gender
+    gender: Gender  # TODO: 생김새로 변경 -> 프롬프트 영향 X
     tone: Tone
     style: Style
     purpose: Purpose
